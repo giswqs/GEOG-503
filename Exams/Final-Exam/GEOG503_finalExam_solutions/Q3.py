9993ea28ea072c37317ccc643e6a6369164e01ae
Qiusheng Wu <giswqs@gmail.com>
2018-10-21 14:21:40 -0400

    delete files

--- Exams/Final-Exam/GEOG503_finalExam_solutions/Q3.py
+++
@@ -1,4 +0,0 @@
-for i in range(1, 5):
-    print('{0}) Hehe'.format(i))
-    for alpha in ['a', 'b', 'c']:
-        print('\t{0}) Hoho'.format(alpha))
--- Exams/Final-Exam/GEOG503_finalExam_solutions/test.py
+++
@@ -1,9 +0,0 @@
-
-x = 1
-
-while True:
-    print(x)
-    x = x +1
-    if x > 100:
-        break
-
